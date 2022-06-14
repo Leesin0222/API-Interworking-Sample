ec5b6d2e089a14c1fb836993ce17e5b05ea87220
Leesin0222 <dragon02229@naver.com>
2022-06-14 15:57:30 +0900

    ⚡:: RetrofitBuilder 작성

---
+++ sample/app/src/main/java/com/company/sample/remote/RetrofitBuilder.kt
@@ -0,0 +1,18 @@
+package com.company.sample.remote
+
+import com.company.sample.remote.api.GithubApi
+import com.company.sample.util.BASE_URL
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitBuilder {
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(OkHttpClient())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val githubApi = retrofit.create(GithubApi::class.java)
+}
